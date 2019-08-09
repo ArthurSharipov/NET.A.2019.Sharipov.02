0282af080cb93f5749089226fc79dff19c4ead8b
Arthur Sharipov <sharipov.artur96@mail.ru>
2019-08-09 22:59:45 +0300

    Add project files.

---
+++ InsertNumber.Tests/InsertingNumbersTests.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace InsertNumber.Tests
+{
+    public class InsertingNumbersTests
+    {
+        [Test]
+        [TestCase(15, 15, 0, 0, ExpectedResult = 15)]
+        [TestCase(8, 15, 0, 0, ExpectedResult = 9)]
+        [TestCase(8, 15, 3, 8, ExpectedResult = 120)]
+        public int Insert(int numberSource, int numberIn, int i, int j)
+        {
+            var insertingNumbers = new InsertingNumbers();
+            return insertingNumbers.Insert(numberSource, numberIn, i, j);
+        }
+    }
+}
---
+++ InsertNumber/InsertingNumbers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace InsertNumber
+{
+    public class InsertingNumbers
+    {
+        public int Insert(int numberSource, int numberIn, int i, int j)
+        {
+            if (i < 0 || j < 0 || i > 31 || j > 31)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            if (i > j)
+            {
+                throw new ArgumentException();
+            }
+            int medium = ((2 << (j - i)) - 1) << i;
+            return (numberSource & ~medium) | ((numberIn << i) & medium);
+        }
+    }
+}
